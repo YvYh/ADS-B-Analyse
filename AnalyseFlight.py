a485ad53e81b97cd37ccdbe8ac6f93cb232673bf
YU Hong <hong.yu@groupe-esigelec.org>
2018-08-31 15:47:55 +0800

    update:save data by /aircraft/callsigh

--- AnalyseFlight.py
+++ AnalyseFlight.py
@@ -2,31 +2,45 @@ import csv
 import pandas as pd
 import numpy as np
 
-file = open('../../Thales/log.txt', 'r')
+file = open('../log.txt', 'r')
 line = file.readlines(1500)
 title = line[-2:-1]
 col = [x.strip(' ') for x in [i.split('|') for i in line[-2:-1]][0]]
 col = col[1:-1]
 line = file.readline()
+line = [x.strip(' ') for x in line.split('|')][1:-1]
+count_null = 0
+
 while line != "":
 	data_list=[]
-	while (line != title[0]):
-		line = [x.strip(' ') for x in line.split('|')][1:-1]
+	callsign = line[7]
+	data = pd.DataFrame(columns=col)
+	while ((line[7] == callsign)):
 		data_list.append(line)
+		#sub_data = pd.DataFrame(data_list, columns=col)
+		#data = data.append(sub_data, ignore_index=True)
+		#data_list=[]
 		line = file.readline()
+		line = [x.strip(' ') for x in line.split('|')][1:-1]
+		if (line==[]):
+			for i in range (4):
+				line = file.readline()
+				line = [x.strip(' ') for x in line.split('|')][1:-1]
+			print('Change table')
 
+		
 	data = pd.DataFrame(data_list,columns=col)
-	data = data.iloc[:-2,:]
-
 	for name in ['lat', 'lon','velocity','heading','vertrate','baroaltitude','geoaltitude','lastposupdate','lastcontact']:
 		data[name] = pd.to_numeric(data[name], errors='coerce')
 	
-	callsign = data.iloc[-1]['callsign']
-
 	coor=data.loc[:,['lat','lon','geoaltitude']]
 	coor=coor.dropna(how='any')
-	coor.to_csv('../../Thales/coordinate/{}.csv'.format(callsign), index=False)
-	line = file.readline()
-	line = file.readline()
-
+	if (callsign=="NULL"):
+		callsign = ('N{}'.format(count_null))
+		count_null = count_null+1
+	coor.to_csv('../coordinate/{}.csv'.format(callsign), index=False)
+	print('{} fine'.format(callsign))
+	
+	
+	
 file.close()
