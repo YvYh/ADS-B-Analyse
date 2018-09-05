7b565739b79afee67a5439e2acfce8cadf6f1cd2
YU Hong <hong.yu@groupe-esigelec.org>
2018-09-05 18:19:57 +0800

    add ads-b data to arcgis from existed csv file

---
+++ CsvToLayer.py
@@ -0,0 +1,29 @@
+import arcpy
+from datetime import datetime
+import pandas as pd
+
+def transfer_time(callsign):
+	data = pd.read_csv(r'D:\Thales\ADS-B\coor\{}.csv'.format(callsign))
+	for i in range(len(data.time)):
+		time = datetime.fromtimestamp(data.time[i])
+		data.time[i] = time.isoformat()
+	newPath=r"D:\Thales\ADS-B\coor\NEW{}.csv".format(callsign)
+	data.to_csv(newPath)
+	return newPath
+
+def shp(name):
+	return "{}.shp".format(name)
+
+#To do: Input 
+#callsign="CXA817"
+callsign = arcpy.GetParameterAsText(0)
+csvfile=transfer_time(callsign)
+workspace=r"C:\Users\Hong\Documents\ArcGIS\ADS-B"
+arcpy.env.workspace=workspace
+arcpy.MakeXYEventLayer_management(str(csvfile),"lon","lat",callsign)
+arcpy.FeatureClassToShapefile_conversion(callsign,workspace)
+arcpy.MakeFeatureLayer_management(shp(callsign),callsign)
+arcpy.CheckOutExtension("tracking")
+trackingLayer="{}T".format(callsign)
+arcpy.MakeTrackingLayer_ta(callsign,trackingLayer,"","NOT_ADJUSTED_FOR_DST","COPY_ALL_TO_MEMORY","time","yyyy-MM-dd HH:mm:ss")
+#arcpy.FeatureClassToShapefile_conversion(trackingLayer,workspace)
--- time-transfer.py
+++ time-transfer.py
@@ -3,7 +3,8 @@ import pandas as pd
 time = datetime.fromtimestamp(1535955340)
 #print('%r %r' % (type(time),time))
 
-data = pd.read_csv("../coor/CXA817.csv")
+data = pd.read_csv("../coor/CXA8667.csv")
 for i in range(len(data.time)):
-	data.time[i] = datetime.fromtimestamp(data.time[i])
-data.to_csv("../coor/NEW-CXA817.csv", index=False)
+	time = datetime.fromtimestamp(data.time[i])
+	data.time[i] = time.isoformat()
+data.to_csv("../coor/NEW-CXA8667.csv")
