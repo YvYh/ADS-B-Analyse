c2d2874664b3b51ff30efde1811141b267ead6df
YU Hong <hong.yu@groupe-esigelec.org>
2018-09-05 18:20:34 +0800

    add real-time ads-b data into arcgis

--- api_test.py
+++ api_test.py
@@ -1,8 +1,33 @@
 from opensky_api import OpenSkyApi
+import time as t
+import pandas as pd
+import arcpy
+from datetime import datetime
+
+def shp(name):
+	return "{}.shp".format(name)
 
 api = OpenSkyApi()
-zbaa = (36.0448, 44.0448, 112.3504, 120.3504)
-states = api.get_states(bbox=zbaa)
-print("Time{}\n".format(states.time))
-for s in states.states:
-	print("(\t%r\t%r\t%r)" %(s.callsign, s.longitude, s.latitude))
+ZSPD = (26.0836, 36.0836, 116.4819, 126.4819)
+l = []
+
+fois = 6
+for i in xrange(long(fois)):
+	states = api.get_states(bbox=ZSPD)
+	for s in states.states:
+		time = datetime.fromtimestamp(s.time_position).isoformat()
+		l.append([s.callsign, s.longitude, s.latitude, s.geo_altitude, s.heading, s.icao24, time])
+	t.sleep(30)
+col=['callsign', 'lon','lat','geoaltitude','heading', 'icao24', 'time']
+data=pd.DataFrame(l,columns=col)
+data = data.drop_duplicates(subset=['callsign','lat','lon'])
+time = states.time
+file = 'D:/Thales/ADS-B/coordinate/{}.csv'.format(str(time))
+data.to_csv(file, index=False)
+
+workspace=r"C:\Users\Hong\Documents\ArcGIS\ADS-B"
+arcpy.env.workspace=workspace
+arcpy.MakeXYEventLayer_management(file,"lon","lat",time)
+arcpy.FeatureClassToShapefile_conversion(time,workspace)
+arcpy.MakeFeatureLayer_management(shp(time),time)
+arcpy.MakeTrackingLayer_ta(shp(time),time,"","NOT_ADJUSTED_FOR_DST","COPY_ALL_TO_MEMORY","time","yyyy-MM-dd HH:mm:ss",'','','',"callsign")
