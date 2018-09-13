e3e8e2a415d9139c42cb5f3e40690984bd1599fa
YU Hong <hong.yu@groupe-esigelec.org>
2018-09-13 10:14:29 +0800

    delate adding tracking layer

--- real-timeADS-B.py
+++ real-timeADS-B.py
@@ -11,7 +11,7 @@ def shp(name):
 def box(airport):
 	return {'ZSPD':(24.8286, 34.8286, 116.481, 126.481),
 	'ZSPD_google':(26.0836, 36.0836, 116.4819, 126.4819),
-	'CHINA':(24.8286, 40.8286, 100.481, 125.481)
+	'CHINA':(24.8286, 41.4286, 98.081, 125.081)
 	}.get(airport, ())
 
 api = OpenSkyApi()
@@ -24,9 +24,11 @@ airport = arcpy.GetParameterAsText(1)
 workspace=r"C:\Users\Hong\Documents\ArcGIS\ADS-B"
 arcpy.env.workspace=workspace
 arcpy.env.overwriteOutput = True
-max = long(fois) + 4
+max = long(fois) + 1
 arcpy.SetProgressor("step","Get real-time ADS-B data...", 0, max, 1)
 
+states = api.get_states(bbox=box(airport))
+t.sleep(20)
 for i in xrange(long(fois)):
 	states = api.get_states(bbox=box(airport))
 	arcpy.SetProgressorLabel("Loading {}".format(i))
@@ -49,36 +51,14 @@ try:
 
 	arcpy.SetProgressorLabel("Creating XY event")
 	#layer = os.path.join(workspace,time)
-	xy = arcpy.MakeXYEventLayer_management(file,"lon","lat",time)
+	arcpy.MakeXYEventLayer_management(file,"lon","lat",time)
 	arcpy.FeatureClassToShapefile_conversion(time,workspace)
 	arcpy.SetProgressorPosition()
 	arcpy.AddMessage("Create XY event")
 	
-	arcpy.SetProgressorLabel("Creating tracking layer")
-	tracking = "{}T".format(time)
-	arcpy.MakeTrackingLayer_ta("{}.shp".format(time),tracking,"","NOT_ADJUSTED_FOR_DST","COPY_ALL_TO_MEMORY","time","yyyy-MM-dd HH:mm:ss",'','','',"callsign")
-	arcpy.AddMessage("Create tracking layer")
-	symbology = r"C:\Users\Hong\Documents\ArcGIS\ADS-B\template.lyr"
-	arcpy.ApplySymbologyFromLayer_management(tracking, symbology)
-	arcpy.AddMessage("Set symbology")
-	#arcpy.FeatureClassToShapefile_conversion(tracking,workspace)
-	arcpy.SetProgressorPosition()
-	#arcpy.AddMessage("Create {}.shp".format(tracking))
-	#arcpy.SetParameter(2, layer)
+	shppath = os.path.join(workspace,shp(time))
+	arcpy.MakeFeatureLayer_management(shppath,time)
 	
-	"""
-	arcpy.SetProgressorLabel("Adding tracking layer")
-	mypath = r"Users\Hong\Documents\ArcGIS\ADS-B\map.mxd"
-	mxd = arcpy.mapping.MapDocument(mypath)
-	df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
-	layer = arcpy.mapping.Layer(shp(tracking))
-	arcpy.mapping.AddLayer(df, layer, "AUTO_ARRANGE")
-	arcpy.RefreshActiveView()
-	arcpy.RefreshTOC()
-	mxd.save()
-	x = os.startfile(mypath)
-	arcpy.SetProgressorPosition()
-	"""
 except Exception:
 	e = sys.exc_info()[1]
 	arcpy.AddError(e.args[0])
